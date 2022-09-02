f1f4229cb1af8113e217fd681cabb6f8d8c06961
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-09-02 15:48:19 -0400

    Configuração blade

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,6 @@ class PrincipalController extends Controller
 {
     public function index()
     {
-        return view('site-template');
+        return view('itshome');
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,19 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.
-        styles([
-            'resources/css/bootstrap.css',
-            'resources/css/fonts.css',
-            'resources/css/style.css'
-            ],'public/css/style.css')
+        copyDirectory('resources/fonts','public/fonts');
 
-    .scripts(
-            [
-            'resources/js/bootstrap.js',
-            'resources/js/core.min.js',
-            'resources/js/script.js',
-            'resources/js/3ts2ksMwXvKRuG480KNifJ2_JNM.js'
-
-
-            ],'public/js/scripts.js')
-    .version();
