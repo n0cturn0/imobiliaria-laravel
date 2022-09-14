12e7808e0174d5a5b992794a7117faec3b5567d1
Luiz Augusto <n0cturn0.debian@gmail.com>
2022-09-13 22:32:07 -0400

    criado html de listagem das etiquetas

--- resources/views/livewire/etiqueta-list.blade.php
+++ resources/views/livewire/etiqueta-list.blade.php
@@ -1,3 +1,37 @@
 <div>
-    oioioioioioioioioioioioioi
+    <section class="section section-lg bg-default">
+        <div class="container">
+
+            <div class="row row-30">
+                <div class="col-md-6 col-lg-4">
+                    <!-- Box Modern-->
+                    <article class="box-modern"><span  class="icon box-modern-icon fl-bigmug-line-big104"></span>
+                        <div class="box-modern-main">
+                            <h4 class="box-modern-title">Various Locations</h4>
+                            <p>We have lots of properties in various locations available for purchase.</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <!-- Box Modern-->
+                    <article class="box-modern"><span class="icon box-modern-icon fl-bigmug-line-wallet26"></span>
+                        <div class="box-modern-main">
+                            <h4 class="box-modern-title">No Commission</h4>
+                            <p>Opportunity to acquire a quality apartment for rent without having to pay any commission.</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <!-- Box Modern-->
+                    <article class="box-modern"><span class="icon box-modern-icon fl-bigmug-line-search74"></span>
+                        <div class="box-modern-main">
+                            <h4 class="box-modern-title">View Apartments</h4>
+                            <p>View apartment listings with photos, HD videos, virtual tours, 3D floor plans etc.</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </div>
